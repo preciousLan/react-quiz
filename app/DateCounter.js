03ef2352c14f1fb968b0d5d793c7eb0ab264fb78
precious lan <olanrewaju.faj@gmail.com>
2025-10-19 20:51:00 +0100

    main

---
+++ app/DateCounter.js
@@ -0,0 +1,65 @@
+'use client';
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'des':
+			return { ...state, count: state.count - state.step };
+		case 'inc':
+			return { ...state, count: state.count + state.step };
+		case 'setcount':
+			return { ...state, count: action.payload };
+		case 'reset':
+			return { ...state, count: 0, step: 1 };
+		case 'setStep':
+			return { ...state, step: action.payload };
+		default:
+			throw new Error('unknown action');
+	}
+}
+
+function DateCounter() {
+	const initialState = { count: 0, step: 1 };
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const { count, step } = state;
+
+	// This mutates the date object.
+	const date = new Date();
+	date.setDate(date.getDate() + count);
+
+	const defineCount = function (e) {
+		dispatch({ type: 'setCount', payload: Number(e.target.value) });
+	};
+
+	const defineStep = function (e) {
+		dispatch({ type: 'setStep', payload: Number(e.target.value) });
+	};
+
+	return (
+		<div className='counter'>
+			<div>
+				<input
+					type='range'
+					min='0'
+					max='10'
+					value={step}
+					onChange={defineStep}
+				/>
+				<span>{step}</span>
+			</div>
+
+			<div>
+				<button onClick={() => dispatch({ type: 'des' })}>-</button>
+				<input value={count === 0 ? 'zero' : count} onChange={defineCount} />
+				<button onClick={() => dispatch({ type: 'inc' })}>+</button>
+			</div>
+
+			<p>{date.toDateString()}</p>
+
+			<div>
+				<button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+			</div>
+		</div>
+	);
+}
+export default DateCounter;
